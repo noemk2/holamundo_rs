11864ab49983ed6ca0063587d104360c51c896c8
noe <noemk2@gmail.com>
2021-10-28 00:14:09 -0500

    fix build

--- build.sh
+++ build.sh
@@ -7,14 +7,9 @@ then
 	RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 	cp target/wasm32-unknown-unknown/release/*.wasm ./res/
 	near dev-deploy --wasm_file ./res/*.wasm
-	source ./neardev/dev-account.env
 else
 	mkdir ./res
 	RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 	cp target/wasm32-unknown-unknown/release/*.wasm ./res/
 	near dev-deploy --wasm_file ./res/*.wasm
-	source ./neardev/dev-account.env
 fi
-
-#mkdir ./res ||  RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-#cp target/wasm32-unknown-unknown/release/*.wasm ./res/
